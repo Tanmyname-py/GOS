d9737498892deb5aa058cafa3ad40485fd1ebedc
Tanmyname <sultanbahri2304l@gmail.com>
2025-02-27 01:35:10 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'https://i.pinimg.com/originals/c8/56/4a/c8564a53ab234f0ff5a4f6f5f92f9650.gif'
-
-data = requests.get(url)
-
-with open('file.mp4','wb') as f :
-   f.write(data.content)
